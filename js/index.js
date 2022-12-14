e3ca6b6d10aeeef282ebffb9b72733c0c397b6fe
Paul M. Summitt <psummitt@gmail.com>
2022-12-13 20:59:15 -0600

    1:15

--- js/index.js
+++ js/index.js
@@ -71,31 +71,66 @@ const quizData = [{
 }
 ];
 
-let currentQuestion = 0;
+const questionE1 = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById("submit");
+const answerE1s = document.querySelectorAll('answer');
+
+let currentQuiz = 0;
+let answer = undefined;
+
+loadQuiz();
 
 function loadQuiz() {
-    const currentQuizData = quizData[currentQuiz];
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuestion];
 
     questionE1.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
+
+    currentQuestion++;
     }
 
 function getSelected() {
-    const answers = document.querySelectorAll('answer');
 
-    answers.forEach();
+    let answer = undefined;
+
+    answerE1s.forEach((answerE1) => {
+        if(answerE1.checked) {
+            answer = answerE1.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerE1s.forEach((answerE1) => {
+        answerE1.checked = false;
+    });
 }
 
 submitBtn.addEventListener("click", () => {
-    currentQuizz++;
+    // check to see the answer
+    const answer = getSelected();
 
-    if (currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        // TODO: Show results
-        alert("You finished!  Get yourself a lemonade");
+    if(answer) {
+        if(answer === quizData[currentQuiz], correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quizData.innerHTML = '<h2>You answered ${score}/${quizData.length} questions correctly.</h2>';
+        }
     }
 });
